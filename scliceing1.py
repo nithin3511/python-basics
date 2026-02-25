c52be0ba93f0f894c2f51534a384aa5d03268f5a
sainithin-ds <146003068+nithin3511@users.noreply.github.com>
2026-02-25 21:36:20 +0530

    Add files via upload

---
+++ scliceing1.py
@@ -0,0 +1,30 @@
+s = "PYTHONIC"
+#print(s[0:4])
+
+##rint(s[5::])
+
+#print(s[2:5])
+
+#print(s[1:5])
+
+#print(s[::-1]) # reverse string
+
+a= "ABCDEFGHIJ"
+
+#print(a[0::2]) #Extract every 2nd character
+
+#print(a[1::2])  #Extract characters at odd index positions
+
+#print(s[:4][::-1] + s[4:])#Reverse only first 4 characters
+
+#print(s[2:7])#Extract string from index 2 to second last
+
+#print(a[::-2])#Reverse string but skip every 2nd character
+
+#print(a[:4:-1])#Extract last 5 characters in reverse order
+
+#print(a[7:1:-1])#Extract substring from index 7 to 2 (reverse direction)
+
+#print(s[-1]+s[1:7]+s[0])#Swap first and last character using slicing
+ 
+#print(a[:2] + a[3:5] + a[6:8] + a[9:11])#Remove every 3rd character
